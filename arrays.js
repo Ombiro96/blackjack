df6bb4a484fff74cc735bb3b7dde6a47c949fa45
ombiro96 <bakutarb@gmail.com>
2022-08-28 15:20:58 +0300

    added new files to practice on array use

---
+++ arrays.js
@@ -0,0 +1,19 @@
+// let featuredPosts=["Check out my Netflix clone",
+//  "Here is code for my project",
+//  "I have relaucnhed my portfilio"]
+
+//  console.log(featuredPosts.length)
+// let cards=[7,4]
+
+// cards.push(6)
+// console.log(cards)
+let messages=[
+    "Hey, how's it going?",
+    "I'm good, thank you: How about you?",
+    "All good, Been working on my portfolio lately."
+]
+let newMessage="Same here!"
+messages.push(newMessage)
+console.log(messages)
+messages.pop()
+console.log(messages)
