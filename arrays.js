986a473938f6b1b0526e35758e57c58535bddf81
ombiro96 <bakutarb@gmail.com>
2022-09-25 22:32:26 +0300

    pushing changes

--- arrays.js
+++ arrays.js
@@ -39,10 +39,79 @@
 // 
 
 //obects
-let castle= {
-  listings: "Naivasha bluh bluh",
-  price: 456,
-  images: ["img/castle.png", "img/castle2.png"]
+// let castle= {
+//   listings: "Naivasha bluh bluh",
+//   price: 456,
+//   images: ["img/castle.png", "img/castle2.png"]
+// }
+// console.log(castle.listings)
+// console.log(castle["images"])
+// let person={
+//   name: "Clifford",
+//   age: 25,
+//   country: "Kenya"
+// }
+// console.log(person)
+// let personEl=document.getElementById("person-el")
+// function logdata(){
+//   personEl.textContent=person.name+ " is "+ person.age + " years old " + " and lives in " + person.country + "."
+// }
+// logdata()
+// let age = 76
+// let discount=""
+// if(age<6){
+//   discount="free"
+// }
+// else if(age<18){
+//   discount="child discount"
+// }
+// else if(age<26){
+//   discount="student discount"
+// }
+// else if(age<66){
+//   discount="full price"
+// }
+// else{
+//   discount="senior citizen discount"
+// }
+// console.log(discount)
+// let largeCountries=["Tuvalu","India","USA","Indonesia","Monaco"]
+// console.log("The largest 5 countires in the world are: ")
+// for(let i=0;i<largeCountries.length;i++){
+//   console.log("- " + largeCountries[i])
+// }
+// largeCountries.pop()//removes last in
+// largeCountries.push("Parkistan")
+// largeCountries.shift()
+// largeCountries.unshift("China")
+// console.log("New data")
+// for(let i=0;i<largeCountries.length;i++){
+//   console.log("- " + largeCountries[i])
+// } //lightweight
+// let dayOfTheMonth=13
+// let weekDay="Friday"
+// if(dayOfTheMonth===13&&weekDay==="Friday")
+// {
+//   console.log("Spooky")
+// } This was too easy
+// let hands=["rock","paper","scissors"]
+// function randomHand(){
+// let item=Math.floor(Math.random()*3)
+// return hands[item]
+// }//will do a rock paper scissors api later
+let fruit=["red","orange","red","red","orange"]
+let redShelf=document.getElementById("red-shelf")
+let orangeShelf=document.getElementById("orange-shelf")
+for(let i=0;i<fruit.length;i++){
+  fruit[i]
+  if(fruit[i]==="red"){
+    redShelf.textContent+=fruit[i]+" "
+  }
+  else if(fruit[i]==="orange")
+  {
+    orangeShelf.textContent+=fruit[i]+ " "
+  }
+  else{
+    console.log("Umekosea boss")
+  }
 }
-console.log(castle.listings)
-console.log(castle["images"])
